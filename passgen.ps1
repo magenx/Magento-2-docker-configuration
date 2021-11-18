b5ec690be7fd8626beaee89aad3de560a3057ead
MagenX <magenx@users.noreply.github.com>
2021-11-18 13:40:36 +0000

    Update passgen.ps1

--- passgen.ps1
+++ passgen.ps1
@@ -3,12 +3,11 @@
 Add-Type -AssemblyName System.Web
 $services = @("mariadb","elasticsearch","rabbitmq","redis")
 ForEach($service in $services) {
-if ($service -eq "redis"){
    Write-Output ($service + ' password generated to -> ' + $service + '/' + $service + '_password')
+if ($service -eq "redis"){
    Out-File -InputObject  ('user default on >' + [System.Web.Security.Membership]::GeneratePassword(32,2) + ' ~* &* +@all')  -FilePath $service/${service}_password -NoNewline
  }
 if ($service -ne "redis"){
-   Write-Output ($service + ' password generated to -> ' + $service + '/' + $service + '_password')
    Out-File -InputObject ([System.Web.Security.Membership]::GeneratePassword(32,2)) -FilePath $service/${service}_password -NoNewline
  }
 }
