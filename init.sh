4f0914c40c6cc97152df743c0b935c482d6b3734
MagenX <magenx@users.noreply.github.com>
2022-06-22 12:37:42 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+bash get-docker.sh
+
+mkdir -p ~/.docker/cli-plugins/  
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+-o ~/.docker/cli-plugins/docker-compose  
+chmod +x ~/.docker/cli-plugins/docker-compose
+
+echo "alias doco='docker compose'" >> ~/.bash_profile
+. ~/.bash_profile
+
+apt -y install git
+
+mkdir -p /usr/local/src/magento && cd $_
+git clone https://github.com/magenx/Magento-2-docker-configuration.git .
+
+bash passgen.sh
+
+echo '{ "features": { "buildkit": true } }' > /etc/docker/daemon.json
