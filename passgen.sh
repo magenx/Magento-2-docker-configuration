8a6dfdab899443c82a600d1c10b9bdf424cc7ec0
MagenX <magenx@users.noreply.github.com>
2022-06-22 12:46:59 +0100

    Update passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -11,7 +11,7 @@ done
 
 echo "user default on >$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?=+_[]{}()<>-' | fold -w 32 | head -n 1) ~* &* +@all" > redis/redis_password
 
-echo "REDIS_PASSWORD=$(grep -Po '(?<=>).*(?<= ~)' redis/redis_password)" > env.php.env
-echo "MARIADB_PASSWORD=$(cat mariadb/mariadb_password)" >> env.php.env
-echo "RABBITMQ_PASSWORD=$(cat rabbitmq/rabbitmq_password)" >> env.php.env
-echo "ELASTICSEARCH_PASSWORD=$(cat elasticsearch/elasticsearch_password)" >> env.php.env
+echo "REDIS_PASSWORD=\"$(grep -Po '(?<=>).*(?= ~)' redis/redis_password)\"" > env.php.env
+echo "MARIADB_PASSWORD=\"$(cat mariadb/mariadb_password)\"" >> env.php.env
+echo "RABBITMQ_PASSWORD=\"$(cat rabbitmq/rabbitmq_password)\"" >> env.php.env
+echo "ELASTICSEARCH_PASSWORD=\"$(cat elasticsearch/elasticsearch_password)\"" >> env.php.env
