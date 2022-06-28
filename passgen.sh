22b39e2820b8ad4db98a87b658725d95fe7f46dd
MagenX <magenx@users.noreply.github.com>
2022-06-28 13:54:18 +0100

    Update passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -4,7 +4,7 @@
 # Generate passwords before deployment
 # # ---------------------------------------------------------------------------------------------------------------------#
 
-for service in rabbitmq mariadb elasticsearch
+for service in portainer rabbitmq mariadb elasticsearch
 do
 head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?=+_[]{}()<>-' | fold -w 32 | head -n 1 > ${service}/${service}_password
 done
