fa741f30a79dd1e6241f1105e40dd76989e9be37
MagenX <magenx@users.noreply.github.com>
2022-07-01 19:45:57 +0100

    Update passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -4,14 +4,9 @@
 # Generate passwords before deployment
 # # ---------------------------------------------------------------------------------------------------------------------#
 
-for service in portainer rabbitmq mariadb elasticsearch
+for SERVICE in REDIS RABBITMQ MARIADB ELASTICSEARCH PORTAINER KIBANA
 do
-head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?-' | fold -w 32 | head -n 1 > ${service}/${service}_password
+PASSWORD=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?-' | fold -w 32 | head -n 1)
+sed -i '/generated passwords for services/,$d' .env
+echo "${SERVICE}_PASSWORD=\"${PASSWORD}\"" >> .env
 done
-
-echo "user default on >$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?-' | fold -w 32 | head -n 1) ~* &* +@all" > redis/redis_password
-
-echo "REDIS_PASSWORD=\"$(grep -Po '(?<=>).*(?= ~)' redis/redis_password)\"" > env.php.env
-echo "MARIADB_PASSWORD=\"$(cat mariadb/mariadb_password)\"" >> env.php.env
-echo "RABBITMQ_PASSWORD=\"$(cat rabbitmq/rabbitmq_password)\"" >> env.php.env
-echo "ELASTICSEARCH_PASSWORD=\"$(cat elasticsearch/elasticsearch_password)\"" >> env.php.env
