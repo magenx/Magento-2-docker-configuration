c01ace6de6dc0c73b4155c4746b34906590afb7a
MagenX <magenx@users.noreply.github.com>
2022-07-01 20:14:07 +0100

    Update passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -6,7 +6,7 @@
 
 sed -i '0,/## generated passwords for services/!d' .env
 
-for SERVICE in REDIS RABBITMQ MARIADB ELASTICSEARCH PORTAINER KIBANA
+for SERVICE in REDIS RABBITMQ MARIADB ELASTIC PORTAINER KIBANA
 do
 PASSWORD=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?-' | fold -w 32 | head -n 1)
 echo "${SERVICE}_PASSWORD=\"${PASSWORD}\"" >> .env
