8edd10b6ccd57c93760fed6c6b48622a4326deea
MagenX <magenx@users.noreply.github.com>
2022-07-01 14:44:51 +0100

    Update passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -6,10 +6,10 @@
 
 for service in portainer rabbitmq mariadb elasticsearch
 do
-head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?=+_[]{}()<>-' | fold -w 32 | head -n 1 > ${service}/${service}_password
+head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?-' | fold -w 32 | head -n 1 > ${service}/${service}_password
 done
 
-echo "user default on >$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?=+_[]{}()<>-' | fold -w 32 | head -n 1) ~* &* +@all" > redis/redis_password
+echo "user default on >$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?-' | fold -w 32 | head -n 1) ~* &* +@all" > redis/redis_password
 
 echo "REDIS_PASSWORD=\"$(grep -Po '(?<=>).*(?= ~)' redis/redis_password)\"" > env.php.env
 echo "MARIADB_PASSWORD=\"$(cat mariadb/mariadb_password)\"" >> env.php.env
