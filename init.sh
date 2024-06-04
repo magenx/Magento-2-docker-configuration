fa58ab8cab8f0225f2438da6c028e3f29d83f1c0
MagenX <magenx@users.noreply.github.com>
2024-06-04 21:57:45 +0200

    init script for debian

--- init.sh
+++ init.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-bash get-docker.sh
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
-mkdir -p ~/.docker/cli-plugins/  
-curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
--o ~/.docker/cli-plugins/docker-compose  
-chmod +x ~/.docker/cli-plugins/docker-compose
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+echo "alias for docker compose command = [ doco ]"
 echo "alias doco='docker compose'" >> ~/.bash_profile
 . ~/.bash_profile
 
+echo "generating random passwords ..."
 bash passgen.sh
-
-echo '{ "features": { "buildkit": true } }' > /etc/docker/daemon.json
