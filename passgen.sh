d91470b7f1ff6645100c740dab98b464a7f34e2b
MagenX <magenx@users.noreply.github.com>
2024-06-04 13:57:42 +0200

    Update passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -6,7 +6,7 @@
 
 sed -i '0,/## generated passwords for services/!d' .env
 
-for SERVICE in PORTAINER REDIS RABBITMQ MARIADB ELASTIC
+for SERVICE in PORTAINER REDIS RABBITMQ MARIADB OPENSEARCH
 do
 PASSWORD=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?-' | fold -w 32 | head -n 1)
  if [[ "${SERVICE}" = "PORTAINER" ]]; then
