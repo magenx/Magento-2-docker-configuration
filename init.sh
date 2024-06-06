ec385b6efd631f0ff2d9f32f2828d4f701a1e2b1
MagenX <magenx@users.noreply.github.com>
2024-06-06 12:50:03 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get install ca-certificates curl
+apt update && apt upgrade -y
+apt-get -y install ca-certificates software-properties-common screen ipset vim strace rsyslog git apache2-utils
+
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -12,11 +13,18 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "alias for docker compose command = [ doco ]"
 echo "alias doco='docker compose'" >> ~/.bash_profile
-. ~/.bash_profile
+source ~/.bash_profile
+
+mkdir /opt/magento
+cd /opt/magento
+
+git clone https://github.com/magenx/Magento-2-docker-configuration.git .
+
+vim .env
 
 echo "generating random passwords ..."
 bash passgen.sh
