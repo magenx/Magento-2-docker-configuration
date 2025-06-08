4e3decb48c33af35aa2ab1be3db3da3ac5e79de4
MagenX <magenx@users.noreply.github.com>
2025-06-08 20:00:21 +0200

    Update passgen.sh

--- passgen.sh
+++ passgen.sh
@@ -6,12 +6,8 @@
 
 sed -i '0,/## generated passwords for services/!d' .env
 
-for SERVICE in PORTAINER REDIS RABBITMQ MARIADB OPENSEARCH INDEXER
+for SERVICE in REDIS RABBITMQ MARIADB OPENSEARCH INDEXER
 do
 PASSWORD=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9@%^&?-' | fold -w 32 | head -n 1)
- if [[ "${SERVICE}" = "PORTAINER" ]]; then
-   echo "${SERVICE}_PASSWORD_CLEAR=\"${PASSWORD}\"" >> .env
-   PASSWORD=$(htpasswd -nb -B admin '${PORTAINER_PASSWORD}' | cut -d ":" -f 2)
- fi
 echo "${SERVICE}_PASSWORD='${PASSWORD}'" >> .env
 done
