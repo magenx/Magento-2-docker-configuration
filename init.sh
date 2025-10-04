d989eb3ff93d504fa8be5e6f324019ac00f85899
tmccaff57 <tmccaff57@gmail.com>
2025-10-03 20:19:32 -0400

    Update init.sh/software-properties-common package no longer needed
    The software-properties-common package, which provides the add-apt-repository command for managing software sources, was removed from Debian 13 (Trixie) because the functionality is being replaced by the newer debian.sources file format and APT tools.

--- init.sh
+++ init.sh
@@ -30,7 +30,7 @@ echo ""
 echo "Docker installation"
 export DEBIAN_FRONTEND=noninteractive 
 apt update && apt upgrade -y
-apt-get -y install ca-certificates software-properties-common screen ipset vim strace rsyslog git apache2-utils
+apt-get -y install ca-certificates screen ipset vim strace rsyslog git apache2-utils
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
