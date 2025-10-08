f06e7d2caeafa1d67a6517498eaa3c3562d51ef2
MagenX <magenx@users.noreply.github.com>
2025-10-08 15:00:30 +0200

    fix: Update config
    - magento entrypoint
    - phpmyadmin network
    - additional_contexts docs unclear

--- magento/entrypoint.sh
+++ magento/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-# If no command provided, default to a long-running process
-if [ $# -eq 0 ]; then
-  exec tail -f /dev/null
-fi
-
 # If first argument is "n98", run n98-magerun2
 if [ "$1" = "n98" ]; then
   shift
---
+++ nginx/sites-enabled.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ln -snf /etc/nginx/sites-available/magento2.conf /etc/nginx/sites-enabled/magento2.conf
+ln -snf /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
