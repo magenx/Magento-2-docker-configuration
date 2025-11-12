f62e9c1c0d399247df8b10edd064ce56a98d5664
MagenX <magenx@users.noreply.github.com>
2025-11-12 12:20:29 +0100

    feat: Using simple and secure setup config

--- scripts/data_directory.sh
+++ scripts/data_directory.sh
@@ -4,12 +4,9 @@
 . .env
 
 for dir in \
-  "portainer:109443" \
-  "fail2ban:101000" \
-  "letsencrypt:101000" \
   "redis-cache:106380" \
   "redis-session:106379" \
-  "redis-insight:105540" \
+  "redisinsight:105540" \
   "rabbitmq:105672" \
   "opensearch:109200" \
   "mariadb:103306" \
--- scripts/random_generator.sh
+++ scripts/random_generator.sh
@@ -7,13 +7,13 @@
 
 sed -i '0,/## generated passwords for services/!d' .env
 
-for SERVICE in REDIS RABBITMQ MARIADB OPENSEARCH INDEXER XHGUI_PDO OPENSEARCH_DASHBOARDS
+for SERVICE in REDIS RABBITMQ MARIADB OPENSEARCH INDEXER XHGUI_PDO
 do
 PASSWORD=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9%&?' | fold -w 32 | head -n 1)
 echo "${SERVICE}_PASSWORD='${PASSWORD}'" >> .env
 done
 
-for THIS in ADMIN RABBITMQ PHPMYADMIN OPENSEARCH_DASHBOARDS
+for THIS in ADMIN RABBITMQ PHPMYADMIN
 do
 RANDOM_PATH=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
 echo "${THIS}_PATH='${THIS,,}_${RANDOM_PATH}'" >> .env
