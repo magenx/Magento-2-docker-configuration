7b476a3d05642d1c87526db9a0674c6a421d2170
MagenX <magenx@users.noreply.github.com>
2025-11-22 13:37:21 +0100

    Delete nginx/sites-enabled.sh

--- nginx/sites-enabled.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ln -snf /etc/nginx/sites-available/magento2.conf /etc/nginx/sites-enabled/magento2.conf
-ln -snf /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
