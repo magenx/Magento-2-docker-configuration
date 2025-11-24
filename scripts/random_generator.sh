57cd1291c56102673cefd9f9981a21ab023a7735
MagenX <magenx@users.noreply.github.com>
2025-11-24 17:22:44 +0100

    Generate random PROFILER variable in .env
    Add PROFILER variable generation to .env file

--- scripts/random_generator.sh
+++ scripts/random_generator.sh
@@ -18,3 +18,5 @@ do
 RANDOM_PATH=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
 echo "${THIS}_PATH='${THIS,,}_${RANDOM_PATH}'" >> .env
 done
+
+echo "PROFILER=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)" >> .env
