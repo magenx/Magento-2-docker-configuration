c95e27226d4dadee3be9aa43e8d6d6a44b312bd1
MagenX <magenx@users.noreply.github.com>
2025-12-05 22:51:24 +0100

    Implement composer command handling in entrypoint.sh
    Add support for composer command in entrypoint script

--- magento/entrypoint.sh
+++ magento/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# If first argument is "composer"
+if [ "$1" = "composer" ]; then
+  shift
+  exec ./vendor/composer/composer/bin/composer "$@"
+fi
+
 # If first argument is "n98", run n98-magerun2
 if [ "$1" = "n98" ]; then
   shift
