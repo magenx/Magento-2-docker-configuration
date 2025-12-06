5f5c65796af3e0a7669e5a954eef63af51a3920d
MagenX <magenx@users.noreply.github.com>
2025-12-06 14:45:22 +0100

    Add IMGPROXY_UID to web_directory.sh

--- scripts/web_directory.sh
+++ scripts/web_directory.sh
@@ -5,6 +5,7 @@
 MAGENTO_UID="10${MAGENTO_UID}"
 PHP_UID="10${PHP_UID}"
 NGINX_UID="10${NGINX_UID}"
+IMGPROXY_UID="10${IMGPROXY_UID}"
 
 if [ ! -d "${APP_PATH}/releases" ] || [ -z "$(ls -A ${APP_PATH}/releases)" ]; then
   INSTALLATION_RELEASE="$(date +'%Y%m%d%H%M')"
@@ -22,3 +23,4 @@ find ${APP_PATH} -type f ! -perm 660 -exec chmod 660 {} \;
 setfacl -R -m m:r-X,u:${MAGENTO_UID}:rwX,g:${PHP_UID}:r-X,o::-,d:u:${MAGENTO_UID}:rwX,d:g:${PHP_UID}:r-X,d:o::- ${APP_PATH}/releases
 setfacl -R -m u:${MAGENTO_UID}:rwX,g:${PHP_UID}:rwX,o::-,d:u:${MAGENTO_UID}:rwX,d:g:${PHP_UID}:rwX,d:o::- ${APP_PATH}/{shared/{var,pub/media},public}
 setfacl -R -m u:${NGINX_UID}:r-X,d:u:${NGINX_UID}:r-X ${APP_PATH}/{shared,releases,public}
+setfacl -R -m u:${IMGPROXY_UID}:r-X,d:u:${IMGPROXY_UID}:r-X ${APP_PATH}/shared/pub/media
