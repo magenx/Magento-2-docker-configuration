4cd250421968c0ee4fcac319b5d7ac7b8d79ea2d
MagenX <magenx@users.noreply.github.com>
2025-12-07 12:54:29 +0100

    Remove PHPMYADMIN from random generator script

--- scripts/random_generator.sh
+++ scripts/random_generator.sh
@@ -13,7 +13,7 @@ PASSWORD=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9%&?' | fold -w 32 | head 
 echo "${SERVICE}_PASSWORD='${PASSWORD}'" >> .env
 done
 
-for THIS in ADMIN RABBITMQ PHPMYADMIN
+for THIS in ADMIN RABBITMQ
 do
 RANDOM_PATH=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
 echo "${THIS}_PATH='${THIS,,}_${RANDOM_PATH}'" >> .env
