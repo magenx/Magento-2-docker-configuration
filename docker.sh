e8419acec5f275fa7660ce7365873488f08c1edd
MagenX <magenx@users.noreply.github.com>
2025-12-08 22:35:56 +0100

    Update sysctl configuration for Docker key quotas

--- docker.sh
+++ docker.sh
@@ -81,7 +81,7 @@ bash scripts/get_nginx_config.sh
 find /opt/${BRAND}/docker -type d -exec chmod 2770 {} \;
 find /opt/${BRAND}/docker -type f -exec chmod 660 {} \;
 
-cat >> /etc/sysctl.conf <<EOF
+cat > /etc/sysctl.d/99-docker-maxkeys.conf <<EOF
 # Session key quota fixes for Docker
 kernel.keys.root_maxkeys=2000000
 kernel.keys.root_maxbytes=20000000
@@ -89,4 +89,4 @@ kernel.keys.maxkeys=2000000
 kernel.keys.maxbytes=20000000
 EOF
 
-sysctl -p
+sysctl -p /etc/sysctl.d/99-docker-maxkeys.conf
