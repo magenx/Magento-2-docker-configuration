1ba165864db605d39fc8990af27267477735298e
MagenX <magenx@users.noreply.github.com>
2026-04-25 16:33:30 +0200

    Swap composer and n98 command handling in entrypoint (#228)

--- magento/entrypoint.sh
+++ magento/entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 set -e
 
-# If first argument is "composer", run composer
-if [ "$1" = "composer" ]; then
-  shift
-  php /usr/local/bin/composer "$@"
-  exit $?
-fi
-
 # If first argument is "n98", run n98-magerun2
 if [ "$1" = "n98" ]; then
   shift
-  php /usr/local/bin/n98-magerun2 "$@"
+  exec /bin/sh -c "php /usr/local/bin/n98-magerun2 $*"
+  exit $?
+fi
+
+# If first argument is "composer", run composer
+if [ "$1" = "composer" ]; then
+  shift
+  exec /bin/sh -c "php /usr/local/bin/composer $*"
   exit $?
 fi
 
 # For any other command, default to bin/magento
-php bin/magento "$@"
+exec /bin/sh -c "php bin/magento $*"
