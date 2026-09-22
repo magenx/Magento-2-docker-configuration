bb67d8532bda63c3dece79c83ac3165be8523211
MagenX <magenx@users.noreply.github.com>
2026-09-22 18:17:00 +0200

    Change Redis password variable in install script
    Updated Redis password variable from REDIS_PASSWORD to VALKEY_PASSWORD for session and cache backend configuration.

--- scripts/install_magento.sh
+++ scripts/install_magento.sh
@@ -10,7 +10,7 @@ docker compose run --rm magento composer create-project --repository-url=https:/
 
 # Composer replace bloatware
 sed -i '/"conflict":/ {
-r magento/composer_replace
+r php/config/cli/composer_replace
 N
 }' ${MAGENTO_ROOT_PATH}/${CURRENT_SYMLINK}/composer.json
 
@@ -41,13 +41,13 @@ docker compose run --rm magento setup:install --base-url=https://\${DOMAIN}/ \
  --session-save-redis-port=6379 \
  --session-save-redis-log-level=3 \
  --session-save-redis-db=0 \
- --session-save-redis-password=\${REDIS_PASSWORD} \
+ --session-save-redis-password=\${VALKEY_PASSWORD} \
  --session-save-redis-compression-lib=lz4 \
  --cache-backend=redis \
  --cache-backend-redis-server=cache \
  --cache-backend-redis-port=6380 \
  --cache-backend-redis-db=0 \
- --cache-backend-redis-password=\${REDIS_PASSWORD} \
+ --cache-backend-redis-password=\${VALKEY_PASSWORD} \
  --cache-backend-redis-compress-data=1 \
  --cache-backend-redis-compression-lib=l4z \
  --amqp-host=rabbitmq \
